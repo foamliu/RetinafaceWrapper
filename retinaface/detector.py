93d4b434d9ead5ef12d7a66a7d21561b65715122
Yang Liu <liuyang12@focusmedia.cn>
2019-11-21 11:16:40 +0800

    update

--- retinaface/detector.py
+++ retinaface/detector.py
@@ -6,11 +6,12 @@ import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
 
-from .data import cfg_mnet
-from .layers.functions.prior_box import PriorBox
-from .models.retinaface import RetinaFace
-from .utils.box_utils import decode, decode_landm
-from .utils.nms.py_cpu_nms import py_cpu_nms
+from retinaface.data import cfg_mnet
+from retinaface.layers.functions.prior_box import PriorBox
+from retinaface.models.retinaface import RetinaFace
+from retinaface.utils.box_utils import decode, decode_landm
+from retinaface.utils.nms.py_cpu_nms import py_cpu_nms
+
 
 confidence_threshold = 0.6
 top_k = 5000
@@ -18,107 +19,69 @@ nms_threshold = 0.4
 keep_top_k = 750
 
 
-def check_keys(model, pretrained_state_dict):
-    ckpt_keys = set(pretrained_state_dict.keys())
-    model_keys = set(model.state_dict().keys())
-    used_pretrained_keys = model_keys & ckpt_keys
-    unused_pretrained_keys = ckpt_keys - model_keys
-    missing_keys = model_keys - ckpt_keys
-    print('Missing keys:{}'.format(len(missing_keys)))
-    print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
-    print('Used keys:{}'.format(len(used_pretrained_keys)))
-    assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
-    return True
-
-
-def remove_prefix(state_dict, prefix):
-    ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
-    print('remove prefix \'{}\''.format(prefix))
-    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
-    return {f(key): value for key, value in state_dict.items()}
-
-
-def load_model():
-    pretrained_path = 'weights/mobilenet0.25_Final.pth'
-    print('Loading pretrained model from {}'.format(pretrained_path))
-    model = RetinaFace(cfg=cfg_mnet, phase='test')
-
-    device = torch.cuda.current_device()
-    pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage.cuda(device))
-    if "state_dict" in pretrained_dict.keys():
-        pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
-    else:
-        pretrained_dict = remove_prefix(pretrained_dict, 'module.')
-    check_keys(model, pretrained_dict)
-    model.load_state_dict(pretrained_dict, strict=False)
-    return model
-
-
-net = load_model()
-
-
 def detect(img_raw):
-    # net and model
-    net.eval()
-    print('Finished loading model!')
-    # print(net)
-    cudnn.benchmark = True
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # sets device for model and PyTorch tensors
-    net = net.to(device)
+    with torch.no_grad():
+        from retinaface.model import net
+        net.eval()
+        print('Finished loading model!')
+        # print(net)
+        cudnn.benchmark = True
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # sets device for model and PyTorch tensors
+        net = net.to(device)
 
-    resize = 1
+        resize = 1
 
-    img = np.float32(img_raw)
-    im_height, im_width = img.shape[:2]
-    scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
-    img -= (104, 117, 123)
-    img = img.transpose(2, 0, 1)
-    img = torch.from_numpy(img).unsqueeze(0)
-    img = img.to(device)
-    scale = scale.to(device)
+        img = np.float32(img_raw)
+        im_height, im_width = img.shape[:2]
+        scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
+        img -= (104, 117, 123)
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img).unsqueeze(0)
+        img = img.to(device)
+        scale = scale.to(device)
 
-    tic = time.time()
-    loc, conf, landms = net(img)  # forward pass
-    # print('net forward time: {:.4f}'.format(time.time() - tic))
+        tic = time.time()
+        loc, conf, landms = net(img)  # forward pass
+        # print('net forward time: {:.4f}'.format(time.time() - tic))
 
-    priorbox = PriorBox(cfg_mnet, image_size=(im_height, im_width))
-    priors = priorbox.forward()
-    priors = priors.to(device)
-    prior_data = priors.data
-    boxes = decode(loc.data.squeeze(0), prior_data, cfg_mnet['variance'])
-    boxes = boxes * scale / resize
-    boxes = boxes.cpu().numpy()
-    scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
-    landms = decode_landm(landms.data.squeeze(0), prior_data, cfg_mnet['variance'])
-    scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
-                           img.shape[3], img.shape[2], img.shape[3], img.shape[2],
-                           img.shape[3], img.shape[2]])
-    scale1 = scale1.to(device)
-    landms = landms * scale1 / resize
-    landms = landms.cpu().numpy()
+        priorbox = PriorBox(cfg_mnet, image_size=(im_height, im_width))
+        priors = priorbox.forward()
+        priors = priors.to(device)
+        prior_data = priors.data
+        boxes = decode(loc.data.squeeze(0), prior_data, cfg_mnet['variance'])
+        boxes = boxes * scale / resize
+        boxes = boxes.cpu().numpy()
+        scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+        landms = decode_landm(landms.data.squeeze(0), prior_data, cfg_mnet['variance'])
+        scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                               img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                               img.shape[3], img.shape[2]])
+        scale1 = scale1.to(device)
+        landms = landms * scale1 / resize
+        landms = landms.cpu().numpy()
 
-    # ignore low scores
-    inds = np.where(scores > confidence_threshold)[0]
-    boxes = boxes[inds]
-    landms = landms[inds]
-    scores = scores[inds]
+        # ignore low scores
+        inds = np.where(scores > confidence_threshold)[0]
+        boxes = boxes[inds]
+        landms = landms[inds]
+        scores = scores[inds]
 
-    # keep top-K before NMS
-    order = scores.argsort()[::-1][:top_k]
-    boxes = boxes[order]
-    landms = landms[order]
-    scores = scores[order]
+        # keep top-K before NMS
+        order = scores.argsort()[::-1][:top_k]
+        boxes = boxes[order]
+        landms = landms[order]
+        scores = scores[order]
 
-    # do NMS
-    dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
-    keep = py_cpu_nms(dets, nms_threshold)
-    # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
-    dets = dets[keep, :]
-    landms = landms[keep]
+        # do NMS
+        dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+        keep = py_cpu_nms(dets, nms_threshold)
+        # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
+        dets = dets[keep, :]
+        landms = landms[keep]
 
-    # keep top-K faster NMS
-    dets = dets[:keep_top_k, :]
-    landms = landms[:keep_top_k, :]
+        # keep top-K faster NMS
+        dets = dets[:keep_top_k, :]
+        landms = landms[:keep_top_k, :]
 
-    dets = np.concatenate((dets, landms), axis=1)
-    return dets
+        dets = np.concatenate((dets, landms), axis=1)
+        return dets
